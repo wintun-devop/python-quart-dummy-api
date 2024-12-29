0a5ef293bfec973212b69c539761c95d93aad067
wintun-devop <wintun.hlaing@outlook.com>
2024-12-29 11:53:38 +0630

    register user

--- quart-apis/server/routes/auth/register.py
+++ quart-apis/server/routes/auth/register.py
@@ -1,9 +1,16 @@
 from quart import jsonify,make_response,Blueprint,request
 #import bcrypt
 from server import bcrypt
+#database import
+from server.models.db import db_session
+from server.models import User
+from sqlalchemy.exc import SQLAlchemyError
 
 #import api prefix
 from server.resources.apis_paths import AUTH_REGISTER_API_LINK
+
+
+
 
 #declare blue print
 register_bp = Blueprint('register',__name__,url_prefix=AUTH_REGISTER_API_LINK)
@@ -17,13 +24,19 @@ async def create_user():
         #hasing passsword
         hash_password = bcrypt.generate_password_hash(user_password).decode('utf-8')
         print("hash password",hash_password)
+        add_user = User(email=user_email,password=hash_password,username=user_name)
+        db_session.add(add_user)
+        db_session.commit()
+        result = db_session.query(User).filter_by(email=user_email).first()
+        print("re",result)
         response = {
-            'id':"1",
+            'id':result.id,
             'username':user_name,
             'email':user_email
         }
         return await make_response(jsonify(response),201)
-     except:
+     except SQLAlchemyError as e:
+        print("err",e)
         error={"status":"fail","message":"internal server error"}
         raise error
 
