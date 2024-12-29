e627dce954015bdc9940b365c1c63afe71a4479e
wintun-devop <wintun.hlaing@outlook.com>
2024-12-29 04:56:50 +0000

    upgrade success

--- quart-apis/server/models/migrations/env.py
+++ quart-apis/server/models/migrations/env.py
@@ -4,6 +4,8 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
+import sys 
+import os
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -11,17 +13,18 @@ config = context.config
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
-if config.config_file_name is not None:
-    fileConfig(config.config_file_name)
+#if config.config_file_name is not None:
+#    fileConfig(config.config_file_name)
+fileConfig(config.config_file_name)
 
 # add your model's MetaData object here
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-from __init__ import Base
-target_metadata = Base
-
 #target_metadata = None
+from __init__ import Base
+target_metadata = Base.metadata
+
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
